3158f340bfa5a23829db088dc44dbdb558a4236d
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-10-09 19:25:09 -0700

    Revised Week 2 Day 1 solution

--- draft/week2-testing.js
+++ draft/week2-testing.js
@@ -1,7 +1,22 @@
 // Week 2 Day 1
 /* comparing two arrays */
+/*
+Today's task is to write a function that will check equality between 2 arrays with a particular structure.
+
+The arrays will:
+
+Be of equal size
+Will each contain some number (X) arrays
+Each inner array will contain that same number (X) primitive elements
+All the nested primitives within each array must strictly match to return a true result. Any mismatches will return false.
+*/
+
 
 function compareArr(arr1, arr2) {
   return arr1.every((el, i) => el === arr2[i])
+}
+
+function compareArrOfArr(arr1, arr2) {
+  return arr1.every((arr, i) => compareArr(arr, arr2[i]))
 }
 
