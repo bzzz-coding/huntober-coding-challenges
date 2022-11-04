bf7c5828d11f0b8b7b1d711d7b6c7b4cdeab9002
Tiantian <86077274+bzzz-coding@users.noreply.github.com>
2022-11-04 16:56:18 -0700

    Update 10-25.js

--- standup-challenges/10-25.js
+++ standup-challenges/10-25.js
@@ -46,21 +46,3 @@ console.log(uniqueArrays([[1,2],[4,4],[5,6,6]]),4)
 console.log(uniqueArrays([[1,2],[3,4],[5,6]]),8)
 console.log(uniqueArrays([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
 
-function charToParens(str) {
-  let result = ''
-  for (let char of str) {
-    let paren = str.indexOf(char) === str.lastIndexOf(char) ? '(' : ')'
-    result += paren
-  }
-  return result
-}
-
-function convertStr(str) {
-  let result = ''
-  for (let char of str) {
-    let newChar = char === ' '? ' ' : char === char.toLowerCase() ? char.toUpperCase() : char.toLowerCase()
-    result = newChar + result
-  }
-  return result
-}
-
