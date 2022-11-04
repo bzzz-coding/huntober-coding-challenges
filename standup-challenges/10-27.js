6fe5f4b97d0e9e3b7cca3d8a11a24ac74e0a9394
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-11-03 17:11:23 -0700

    Completed 10-27 challenge

---
+++ standup-challenges/10-27.js
@@ -0,0 +1,25 @@
+/*
+Map (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
+indexOf(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
+lastIndexOf(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf)
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+
+
+From: 
+https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/javascript
+*/
+
+const duplicateEncode = word => word
+  .toLowerCase()
+  .split('')
+  .map(function (letter, index, wordArr) {
+    return wordArr.indexOf(letter) == wordArr.lastIndexOf(letter) ? '(' : ')'
+  })
+  .join('')
