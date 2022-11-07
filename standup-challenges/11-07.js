104d17548e3a1db1767426b66163ef9dff1956da
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-11-07 14:37:57 -0800

    Completed 11-07 challenge

---
+++ standup-challenges/11-07.js
@@ -0,0 +1,32 @@
+/*
+This problem has lots of gotchas when considering time complexity! Def. don't miss this standup if you want to get better at technical interviews! 
+
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the array and no difference with it, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+
+findDeletedNumber([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion')
+findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion')
+findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
+*/
+
+function findDeletedNumber(arr, mixArr) {
+  let mixNumsObj = mixArr.reduce((obj, current) => {
+    if (obj[current] === undefined) {
+      obj[current] = true  
+    }
+    return obj 
+  }, {})
+  for (let num of arr) {
+    if (!mixNumsObj[num]) return num
+  }
+  return 0
+}
+
