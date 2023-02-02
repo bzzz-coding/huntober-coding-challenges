8585a86b8c8ce6451cb25133869cf0eb7ff12a55
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-02-02 13:29:13 -0800

    Added recursive solution to 1-31 challenge

--- standup-challenges/1-31-23.js
+++ standup-challenges/1-31-23.js
@@ -5,8 +5,8 @@ The inputs x and y are always greater or equal to 1, so the greatest common divi
 */
 
 function mygcd(x,y){
-  let lesser = Math.min(x, y)
-  for (let i = lesser; i > 0; i--) {
+  let smallerInt = Math.min(x, y)
+  for (let i = smallerInt; i > 0; i--) {
     if (x % i === 0 && y % i === 0) {
       return i
     }
@@ -14,3 +14,24 @@ function mygcd(x,y){
 }
 
 console.log(mygcd(30,12),6)
+
+function recGCD(x, y) {
+  let bigger = x >= y ? x : y;
+  let smaller = x <= y ? x : y;
+
+  if (smaller === 0) return bigger
+  return recGCD(smaller, bigger % smaller)
+}
+
+console.log(recGCD(30, 12), 6)
+console.log(recGCD(12, 30), 6)
+console.log(recGCD(48, 14), 2)
+console.log(recGCD(14, 48), 2)
+
+// 48, 14
+// first, get the remainder of biggerInt % smallerInt
+// 48 = 14 * 3 + 6  
+// then, get remainder of smallerInt % remainder from the previous calculation
+// 14 = 6 * 2 + 2
+// repeat until we have a remainder of 0, then, the GCD is the previous remainder, in this case, 2
+// 6 = 2 * 3 + 0
